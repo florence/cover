e4a7f0701f282c3c8da8628fc0090a49151141eb
Ryan Plessner <plessner.ryan@gmail.com>
2014-12-28 16:00:05 -0500

    added a directory command and a coverage command

--- raco.rkt
+++ raco.rkt
@@ -1,14 +1,30 @@
 #lang racket
-(require raco/command-name "main.rkt")
+(require raco/command-name "main.rkt" "format.rkt")
 
 (module+ main
+  
+  (define coverage-dir "coverage")
+  (define coverage? #f)
+  (define output-format "")
+  
   (define files
     (expand-directories
      (command-line
       #:program (short-program+command-name)
-      #:args files files)))
+      #:once-any
+      [("-d" "--directory") d "Specify output directory" (set! coverage-dir d)]
+      [("-c" "--coverage") format
+                           "Specify that coverage should be run and optional what format" 
+                           (set! coverage? #t) 
+                           (set! output-format format)]
+      #:args files 
+      files)))
   (printf "testing ~s\n" files)
-  (apply test-files! files))
+  (apply test-files! files)
+  (when coverage?
+    (printf "COVERAGE!")
+    (case output-format
+      [("html") (generate-html-coverage (get-test-coverage) coverage-dir)])))
 
 ;; TODO allow for arbitrary extensions
 (define extensions '(#rx".rkt$" #rx".ss$"))
