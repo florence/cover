abfc9c8955cc4ae238f0b84acd5f38e2fa1936e7
Spencer Florence <florence@ccs.neu.edu>
2015-09-04 16:24:28 -0500

    changed sorting dir

--- cover/private/html/assets/app.js
+++ cover/private/html/assets/app.js
@@ -63,5 +63,5 @@ window.onload = function() {
   createSorter('covered-expressions', 'up', floatCompareAsc, floatCompareDesc);
   createSorter('uncovered-expressions', 'up', floatCompareAsc, floatCompareDesc);
   createSorter('total-expressions', 'up', floatCompareAsc, floatCompareDesc);
-  sortTable('coverage-percentage', stringCompareDesc);
+  sortTable('coverage-percentage', floatCompareAsc);
 };
