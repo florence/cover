5acbb1288e6870f20277b34c7ba4542f3d8c235b
Spencer Florence <spencer@florence.io>
2016-01-11 16:22:03 -0600

    fixed directory creation in raw format

--- cover/private/raw.rkt
+++ cover/private/raw.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
-(require racket/pretty "../cover.rkt")
+(require racket/pretty racket/file "../cover.rkt")
 (provide generate-raw-coverage)
 (define (generate-raw-coverage coverage files [dir "coverage"])
+  (make-directory* dir)
   (with-output-to-file (build-path dir "coverage.rktl")
     #:exists 'replace
     (lambda () (pretty-write (coverage-wrapper-map coverage)))))
