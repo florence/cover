578f4ebd0dee8b62e2e9325fbd7632c4b73499fe
Spencer Florence <spencer@florence.io>
2016-02-29 13:53:20 -0600

    inc version number

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define name "cover")
 (define collection 'multi)
 
-(define version "3.0.2")
+(define version "3.0.3")
 
 (define deps '(("base" #:version "6.1.1") "errortrace-lib" "rackunit-lib"
                "syntax-color-lib" "compiler-lib" "custom-load" "data-lib"))
