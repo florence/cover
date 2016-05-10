2c5bf77f1c28d2146ac7cf748b1de7b946dbc3c7
Spencer Florence <spencer@florence.io>
2016-05-09 21:49:58 -0500

    fix version dep on base

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 
 (define version "3.1.0")
 
-(define deps '(("base" #:version "6.1.1") ("errortrace-lib" #:version "1.1")
+(define deps '(("base" #:version "6.5.0.4") ("errortrace-lib" #:version "1.1")
                "rackunit-lib" "syntax-color-lib" "compiler-lib"
                "custom-load" "data-lib"))
 
