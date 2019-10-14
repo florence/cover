a9e9889c0f0f6de36ec88590077a042d09671fb6
Spencer Florence <spencer@florence.io>
2019-10-14 16:15:04 -0500

    bump version

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define name "cover")
 (define collection 'multi)
 
-(define version "3.2.0")
+(define version "3.2.1")
 
 (define deps '(("base" #:version "6.9") ("errortrace-lib" #:version "1.1")
                "rackunit-lib" "syntax-color-lib" "compiler-lib"
