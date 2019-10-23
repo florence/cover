f2b048d5c2fe24447f0ad22c964178ce634e86ed
Spencer Florence <spencer@florence.io>
2019-10-22 19:19:52 -0500

    .

--- cover/private/contracts.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-(provide coverage/c coverage-gen/c)
-(require racket/contract)
-
-;; if its a file path, will be an absolute path
-(define coverage/c (-> any/c exact-positive-integer?
-                       (or/c 'covered 'uncovered 'irrelevant)))
-(define coverage-gen/c (->* (coverage/c (listof path-string?)) (path-string?) any))
