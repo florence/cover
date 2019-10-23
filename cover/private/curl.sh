356a7f0c0ccdbc1521b8cc078ccc16b512edc116
Spencer Florence <spencer@florence.io>
2019-10-22 20:40:31 -0500

    .

--- cover/private/curl.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-file=$1
-verbose=$2
-
-curl $verbose --include --fail --form json_file=@"$file" "https://coveralls.io/api/v1/jobs"
--- cover/tests/eval/eval.rkt
+++
@@ -1,4 +0,0 @@
-#lang racket
-(require (for-syntax "eval-call.rkt"))
-(begin-for-syntax
-  (eval-the-thing #'(module x racket 0)))
