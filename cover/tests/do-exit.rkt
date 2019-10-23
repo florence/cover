e06ef0b80b784e2b04124785b8741772ccb2f64a
Spencer Florence <spencer@florence.io>
2019-10-22 19:26:09 -0500

    .

--- cover/tests/do-exit.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket/base
-(require "../main.rkt" racket/runtime-path)
-(define-runtime-path exit.rkt "exit.rkt")
-(parameterize ([current-cover-environment (make-cover-environment)])
-  (test-files! exit.rkt))
