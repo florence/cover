877c2b34498347bc51dc2f20040738125675d3fa
Spencer Florence <spencer@florence.io>
2019-10-22 19:32:27 -0500

    .

--- cover/tests/do-eval.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket/base
-(require racket/runtime-path "../main.rkt" rackunit)
-(define-runtime-path eval.rkt "eval.rkt")
-(parameterize ([current-cover-environment (make-cover-environment)])
-  (check-true (test-files! eval.rkt)))
