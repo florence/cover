4122ba9282e110141879da1445e38da62d61a3c8
Spencer Florence <spencer@florence.io>
2019-10-22 20:23:39 -0500

    .

--- cover/tests/eval/do-eval.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(require cover racket/runtime-path rackunit)
-(define-runtime-path eval.rkt "eval.rkt")
-(define-runtime-path eval-call.rkt "eval-call.rkt")
-(check-not-exn (lambda () (test-files!
-                           (path->string eval-call.rkt)
-                           (path->string eval.rkt))))
