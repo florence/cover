8595dfe4be5b6273c27c22008b1a915d3089b8f3
Spencer Florence <spencer@florence.io>
2019-10-22 19:59:41 -0500

    .

--- cover/tests/do-dont-run.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket/base
-(require rackunit "../main.rkt" racket/runtime-path racket/port)
-(define-runtime-path dont-run.rkt "dont-run.rkt")
-(check-not-exn
- (lambda ()
-   (check-true (test-files! (path->string dont-run.rkt)))))
--- info.rkt
+++ info.rkt
@@ -3,8 +3,6 @@
 (define name "cover")
 (define collection 'multi)
 
-(define version "3.3.0")
-
 (define deps '(("base" #:version "6.9") ("errortrace-lib" #:version "1.1")
                "rackunit-lib" "syntax-color-lib" "compiler-lib"
                "custom-load" "data-lib"))
@@ -13,5 +11,3 @@
   '("racket-doc" "scribble-lib" "typed-racket-doc" "htdp-lib"
     "net-doc" "scribble-doc" "at-exp-lib" "scheme-lib" "typed-racket-lib"
     "macro-debugger"))
-
-(define pkg-authors '("spencer@florence.io"))
